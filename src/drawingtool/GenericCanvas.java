65bea2bacb4d85a0a9413c3e813ec3b076d61904
João Marcos Silva <joaomarcos50763@gmail.com>
2020-06-03 13:25:43 -0300

    Add option to use Canvas as a component background

---
+++ src/drawingtool/GenericCanvas.java
@@ -0,0 +1,52 @@
+package drawingtool;
+
+import java.awt.Color;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author Joao
+ */
+public class GenericCanvas {
+
+    private Canvas canvas = new Canvas();
+    private JComponent parentComp;
+
+    public GenericCanvas(JComponent parentComp) {
+        this.parentComp = parentComp;
+        initCanvas();
+    }
+
+    private void initCanvas() {
+        //Adds Canvas within the component
+        this.parentComp.add(canvas);
+        //Leaves the canvas transparent
+        canvas.setOpaque(false);
+        /*Adds the event responsible for resizing 
+        the canvas to the size of the component*/
+        this.parentComp.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent evt) {
+                GenericCanvas.this.setCanvasSize();
+            }
+        });
+
+        this.setCanvasSize();
+    }
+
+    private void setCanvasSize() {
+        canvas.setBounds(0, 0, this.parentComp.getWidth(),
+                this.parentComp.getHeight());
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
+}
